0046e2c3fccbd1a6caeda20f70a3e5e9ad695169
Rifki Martleo Alfiansyah <rifkimartleoa@student.uns.ac.id>
2025-05-14 14:44:38 +0700

    app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-import io
-# import tensorflow as tf  # Nonaktifkan sementara
 
 # ======== Konfigurasi Halaman ========
 st.set_page_config(
